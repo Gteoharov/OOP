9a2603a22d168930e57a4fd109f8c1956ecb4f7a
Gteoharov <georgia.teoharov@gmail.com>
2019-07-21 23:58:32 +0300

    making more abstract

---
+++ Exercises-Working-with-Abstraction-Exercises-Resources/P01_RawData/CarCatalog.cs
@@ -0,0 +1,44 @@
+﻿
+namespace P01_RawData
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class CarCatalog
+    {
+        private List<Car> cars;
+
+        public CarCatalog()
+        {
+            this.cars = new List<Car>();
+        }
+
+        public void Add(string[] parameters)
+        {
+            string model = parameters[0];
+            int engineSpeed = int.Parse(parameters[1]);
+            int enginePower = int.Parse(parameters[2]);
+            int cargoWeight = int.Parse(parameters[3]);
+            string cargoType = parameters[4];
+
+            Engine engine = new Engine(engineSpeed, enginePower);
+            Cargo cargo = new Cargo(cargoWeight, cargoType);
+            Tire[] tires = new Tire[4];
+
+            int indexOfTire = 0;
+            for (int indexTire = 5; indexTire <= 12; indexTire++)
+            {
+                double tirePressure = double.Parse(parameters[indexTire]);
+                int tireAge = int.Parse(parameters[indexTire]);
+
+                Tire tire = new Tire(tirePressure, tireAge);
+                tires[indexOfTire] = tire;
+                indexOfTire++;
+            }
+
+            Car car = new Car(model, engine, cargo, tires);
+
+            cars.Add(car);
+        }
+    }
+}
