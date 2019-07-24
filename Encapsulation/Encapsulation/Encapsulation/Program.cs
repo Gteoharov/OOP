69e3c867c2c48c3b2e349d7a84a4020dd4e21570
Gteoharov <georgia.teoharov@gmail.com>
2019-07-25 00:26:23 +0300

    encapsulation

--- Encapsulation/Encapsulation/Encapsulation/Program.cs
+++ Encapsulation/Encapsulation/Encapsulation/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
-
-namespace Encapsulation
+﻿namespace Encapsulation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var lines = int.Parse(Console.ReadLine());
+            var peoples = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var cmdArgs = Console.ReadLine().Split();
+                var firstName = cmdArgs[0];
+                var lastName = cmdArgs[1];
+                var age = int.Parse(cmdArgs[2]);
+                var salary = decimal.Parse(cmdArgs[3]);
+
+                var person = new Person(firstName, lastName, age, salary);
+                peoples.Add(person);
+            }
+
+            var sorted = peoples.OrderBy(x => x.FirstName).ThenBy(x => x.Age).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                Console.WriteLine(sorted[i]);
+            }
         }
     }
 }
