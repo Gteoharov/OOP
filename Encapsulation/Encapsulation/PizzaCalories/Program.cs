baa095ca8ae3705f32b91506bca707da08561d39
Gteoharov <georgia.teoharov@gmail.com>
2019-07-26 23:59:14 +0300

    Pizza Calories

---
+++ Encapsulation/Encapsulation/PizzaCalories/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PizzaCalories
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
